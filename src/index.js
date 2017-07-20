0d33547136740d77da91efe0a4cb15c285e48bf7
matt-sm <matt-sm@users.noreply.github.com>
2017-07-20 10:03:56 +1000

    add redux-form (#3)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import todoApp from './reducers'
+import store from './reducers'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
-
-let store = createStore(todoApp)
 
 render(
   <Provider store={store}>
