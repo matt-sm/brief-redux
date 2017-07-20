2468e66dc0d3efaed43f09a3ccdc984b1b12a6f3
matt-sm <matt-sm@users.noreply.github.com>
2017-07-20 19:59:20 +1000

    add field validation (#4)

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
+const required = value => (value ? undefined : 'Required')
+
+const renderField = ({
+  input,
+  label,
+  type,
+  meta: { touched, error }
+}) =>
+  <div>
+    <label>
+      {label}
+    </label>
+    <div>
+      <input {...input} placeholder={label} type={type} />
+      {touched &&
+        (error &&
+          <span>
+            {error}
+          </span>)}
+    </div>
+  </div>
+
 let AddTodoForm = ({ handleSubmit }) => {
   return (
     <div>
       <form onSubmit={ handleSubmit }>
         <label htmlFor="name">Name</label>
-        <Field name="name" component="input" type="text" />
+        <Field name="name" component={renderField} type="text" validate={required}/>
         <button type="submit">
           Add Todo
         </button>
@@ -16,7 +38,6 @@ let AddTodoForm = ({ handleSubmit }) => {
 }
 
 AddTodoForm = reduxForm({
-  // a unique name for the form
   form: 'addtodo'
 })(AddTodoForm)
 
